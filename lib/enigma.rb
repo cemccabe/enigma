51c2c62ee94324fbd6b52e6882aa81a800c7dc2a
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 15:16:10 -0700

    Create #shift method; Pass respective test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -16,4 +16,13 @@ class Enigma
   def date_offset(date)
     (date.to_i ** 2).digits.reverse.last(4)
   end
+
+  def shift(key, date)
+    date_shift = date_offset(date)
+    key.chars.filter_map.with_index do |single, index|
+      if index <= 3
+        (([single, key.chars[index + 1]].join.to_i) + date_shift[index]) % 27
+      end
+    end
+  end
 end

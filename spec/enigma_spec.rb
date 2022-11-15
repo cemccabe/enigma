f37294de8dad242de1e3bb8e77df28ad0faeb420
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 13:52:42 -0700

    Create test for #character_set

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe Enigma do
       date: "040895"
     })
   end
+
+  it 'makes a character set' do
+    expect(@enigma.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
 end

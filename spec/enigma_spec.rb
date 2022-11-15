2c15c671bd1dd850910db5f1a823acadad735528
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 13:44:57 -0700

    Create test for encryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(@enigma).to be_a(Enigma)
   end
+
+  it 'encrypts a string' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
+  end
 end

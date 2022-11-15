9015a109ad64e6d20f817aaf3f02b545454eea2b
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 15:30:09 -0700

    Add #encrypt method; Pass respective test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -25,4 +25,14 @@ class Enigma
       end
     end
   end
+
+  def encrypt(message, key = make_random_key, date = date_format)
+    split_up_message = message.downcase.chars
+    make_key = shift(key, date)
+    encrypt = split_up_message.map.with_index do |char, index|
+      new_index = (character_set.index(char) + make_key[index % 4]) % 27
+      character_set[new_index]
+    end
+    {encryption: encrypt.join, key: key, date: date}
+  end
 end

6eb9574d54eb09583a0fd6a5abda40b6ac3fa300
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 14:55:16 -0700

    Add #date_format method; Pass respective test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class Enigma
   def character_set
     ("a".."z").to_a << " "
@@ -5,5 +7,9 @@ class Enigma
 
   def make_random_key
     rand.to_s[2..6]
+  end
+
+  def date_format
+    Time.now.strftime("%d%m%y").to_s.delete('/')
   end
 end

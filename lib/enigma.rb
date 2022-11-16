566f957f5cc32634ebbc14885448efeb1695c9f6
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-15 17:16:22 -0700

    Create MakeMove module; Create MakeMove spec file; Initialize MakeMove in enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,31 +1,9 @@
 require 'time'
+require_relative 'make_move'
 
 class Enigma
-  def character_set
-    ("a".."z").to_a << " "
-  end
-
-  def make_random_key
-    rand.to_s[2..6]
-  end
-
-  def date_format
-    Time.now.strftime("%d%m%y").to_s.delete('/')
-  end
-
-  def date_offset(date)
-    (date.to_i ** 2).digits.reverse.last(4)
-  end
-
-  def shift(key, date)
-    date_shift = date_offset(date)
-    key.chars.filter_map.with_index do |single, index|
-      if index <= 3
-        (([single, key.chars[index + 1]].join.to_i) + date_shift[index]) % 27
-      end
-    end
-  end
-
+  include MakeMove
+  
   def encrypt(message, key = make_random_key, date = date_format)
     split_up_message = message.downcase.chars
     make_key = shift(key, date)
---
+++ spec/make_move_spec.rb
@@ -0,0 +1,31 @@
+require './lib/enigma'
+require './lib/make_move'
+
+RSpec.describe MakeMove do
+  before :each do
+    @enigma = Enigma.new
+    @date = Time.now.strftime("%d%m%y").to_s.delete("/")
+  end
+
+  it 'makes a character set' do
+    expect(@enigma.character_set).to be_a(Array)
+    expect(@enigma.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'makes a random key' do
+    expect(@enigma.make_random_key).to be_a(String)
+    expect(@enigma.make_random_key.length).to eq(5)
+  end
+
+  it 'formats the date to be used for offsets' do
+    expect(@enigma.date_format).to eq(@date)
+  end
+
+  it 'creates the offsets from the formatted date' do
+    expect(@enigma.date_offset('040895')).to eq([1, 0, 2, 5])
+  end
+
+  it 'creates the shift amount for A, B, C, D shifts' do
+    expect(@enigma.shift("02715", "040895")).to eq([3, 0, 19, 20])
+  end
+end
